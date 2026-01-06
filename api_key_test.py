9123f1127952840ba8a8ac478bcbc3073e561938
Jibril Yahaya Jibril <148794521+Jhay001@users.noreply.github.com>
2026-01-06 06:38:13 +0000

    Renamed file to api_key_test

---
+++ api_key_test.py
@@ -0,0 +1,10 @@
+# importing the os module
+import os
+
+# importing load_dotenv function from dotenv module
+from dotenv import load_dotenv
+load_dotenv()
+
+# Getting and printing API key
+api_key = os.getenv("GOOGLE_API_KEY")
+print(f"API_KEY: {api_key}")
