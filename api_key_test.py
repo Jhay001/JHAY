00e3a71e68acf61ab7b44b4ddbc6b71dbfdb4aa4
Jibril Yahaya Jibril <148794521+Jhay001@users.noreply.github.com>
2026-01-06 09:12:46 +0000

    Updated Key name from GOOGLE_API_KEY to GEMINI_API_KEY

--- api_key_test.py
+++ api_key_test.py
@@ -6,5 +6,5 @@ from dotenv import load_dotenv # type: ignore
 load_dotenv()
 
 # Getting and printing API key
-api_key = os.getenv("GOOGLE_API_KEY")
+api_key = os.getenv("GEMINI_API_KEY")
 print(f"API_KEY: {api_key}")
